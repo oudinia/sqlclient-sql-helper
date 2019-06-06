6b0ccdffdbc70390157933fe1aa0f3efe27954a7
unknown <oussama.dinia@engie.com>
2019-06-06 14:50:21 +0000

    Initial Commit

---
+++ SQLHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class SqlHelper
+    {
+        public static int ExecuteNonQuery(SqlConnection conn, string cmdText, SqlParameter[] cmdParams)
+        {
+            var cmd = conn.CreateCommand();
+            PrepareCommand(cmd, conn, null, CommandType.Text, cmdText, cmdParams);
+            var val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        public static int ExecuteNonQuery(SqlConnection conn, string cmdText)
+        {
+            var cmd = conn.CreateCommand();
+            PrepareCommand(cmd, conn, null, CommandType.Text, cmdText);
+            var val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        public static int ExecuteNonQuery(SqlConnection conn, CommandType cmdType, string cmdText, SqlParameter[] cmdParams)
+        {
+            var cmd = conn.CreateCommand();
+            using (conn)
+            {
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParams);
+                var val = cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+
+        public static SqlDataReader ExecuteReader(SqlConnection conn, CommandType cmdType, string cmdText, SqlParameter[] cmdParams)
+        {
+            var cmd = conn.CreateCommand();
+            PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParams);
+            var rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return rdr;
+        }
+
+        public static SqlDataReader ExecuteReader(SqlConnection conn, CommandType cmdType, string cmdText)
+        {
+            var cmd = conn.CreateCommand();
+            PrepareCommand(cmd, conn, null, cmdType, cmdText);
+            var rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return rdr;
+        }
+
+        public static object ExecuteScalar(SqlConnection conn, CommandType cmdType, string cmdText, SqlParameter[] cmdParams)
+        {
+            var cmd = conn.CreateCommand();
+            PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParams);
+            var val = cmd.ExecuteScalar();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        private static void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType cmdType, string cmdText, SqlParameter[] commandParameters)
+        {
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+            }
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            if (trans != null)
+            {
+                cmd.Transaction = trans;
+            }
+            cmd.CommandType = cmdType;
+            //attach the command parameters if they are provided
+            if (commandParameters != null)
+            {
+                AttachParameters(cmd, commandParameters);
+            }
+        }
+
+        private static void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType cmdType, string cmdText)
+        {
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+            }
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            if (trans != null)
+            {
+                cmd.Transaction = trans;
+            }
+            cmd.CommandType = cmdType;
+        }
+        private static void AttachParameters(SqlCommand command, SqlParameter[] commandParameters)
+        {
+            foreach (var p in commandParameters)
+            {
+                //check for derived output value with no value assigned
+                if ((p.Direction == ParameterDirection.InputOutput) && (p.Value == null))
+                {
+                    p.Value = DBNull.Value;
+                }
+                command.Parameters.Add(p);
+            }
+        }
+    }
+}
